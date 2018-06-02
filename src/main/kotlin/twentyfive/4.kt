1618d947c2682816d5edbbd93a7d99cc8a110e9d
Emre <emrsmsrli1@gmail.com>
2018-06-03 00:56:02 +0300

    remove @inner label from a for in maxPalindrome()

--- src/main/kotlin/twentyfive/4.kt
+++ src/main/kotlin/twentyfive/4.kt
@@ -7,7 +7,7 @@ import util.isPalindrome
 fun maxPalindrome(from: Int = 100, to: Int = 999): Int {
     var max = -1
     for(i in to downTo from) {
-        inner@ for(j in to downTo from) {
+        for(j in to downTo from) {
             val num = i * j
             if(num.toString().isPalindrome() && num > max)
                 max = num
