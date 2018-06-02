4279ec9369e13a13439dd1a0b93bf98400de76dd
Emre <emrsmsrli1@gmail.com>
2018-06-02 23:02:44 +0300

    fix palindrome edge values are inverted

--- src/main/kotlin/twentyfive/4.kt
+++ src/main/kotlin/twentyfive/4.kt
@@ -6,8 +6,8 @@ import util.isPalindrome
 
 fun maxPalindome(from: Int = 100, to: Int = 999): Int {
     var max = -1
-    for(i in from downTo to) {
-        inner@ for(j in from downTo to) {
+    for(i in to downTo from) {
+        inner@ for(j in to downTo from) {
             val num = i * j
             if(num.toString().isPalindrome() && num > max)
                 max = num
