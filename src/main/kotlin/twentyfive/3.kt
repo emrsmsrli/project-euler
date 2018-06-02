fc2d263f85929189b375d3adf8d591bd99359f7e
Emre <emrsmsrli1@gmail.com>
2018-06-02 21:49:00 +0300

    remove main() and put default arg to largestFactor()

--- src/main/kotlin/twentyfive/3.kt
+++ src/main/kotlin/twentyfive/3.kt
@@ -4,11 +4,7 @@ package twentyfive
 
 import util.sqrt
 
-fun main(args: Array<String>) {
-    print(largestFactor(600851475143L))
-}
-
-tailrec fun largestFactor(n: Long): Long {
+tailrec fun largestFactor(n: Long = 600851475143L): Long {
     val factor = smallestFactor(n)
     if(factor >= n)
         return n
